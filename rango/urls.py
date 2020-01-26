ea5a007ae6bc297e21f8c276adbcc77042ac9e3b
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2020-01-26 00:01:56 +0000

    created the about view

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from rango import views
 app_name = 'rango'
 urlpatterns = [
-path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name ='about')
 ]
