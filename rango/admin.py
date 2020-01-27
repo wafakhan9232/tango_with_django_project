eb35f9d135d2d5d5b78b986fe55edb4768c3e85b
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2020-01-27 21:12:26 +0000

    Completed TWD Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from rango.models import Category, Page
+admin.site.register(Category)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category', 'url')
+admin.site.register(Page, PageAdmin)
