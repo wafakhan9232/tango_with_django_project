51d086e3031db8f6ce8493d6902b9d8520311428
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2020-02-07 10:55:48 +0000

    Completed TWD Exercise 9.6

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -47,6 +47,13 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+PASSWORD_HASHERS = [
+	'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+	'django.contrib.auth.hashers.BCryptPasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
